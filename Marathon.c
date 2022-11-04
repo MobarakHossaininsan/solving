1f89287154d7eefabdc098425f8aea72e30a80e3
MobarakHossaininsan <mobarakhossaininsan17@gmail.com>
2022-11-04 22:06:51 +0600

    Marathon

---
+++ Marathon.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b,c,d;
+    scanf("%d",&n);
+    while(n--)
+    {
+        scanf("%d%d%d%d",&a,&b,&c,&d);
+         if(a<b && a<c && a<d)
+             printf("3\n");
+        else if(a<b && (a<c || a<d))
+         printf("2\n");
+         else if((a<b || a<c) && a<d)
+         printf("2\n");
+         else if(a<c && (a<b || a<d))
+         printf("2\n");
+        else if(a<b || a<c || a<d)
+            printf("1\n");
+        else
+             printf("0\n");
+
+    }
+    return 0;
+}
